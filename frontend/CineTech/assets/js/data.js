695ed2ab1350f80bb0e2240ddfd33edf081a553c
TUF_garoa <raulvilela29@gmail.com>
2025-03-30 22:46:01 -0400

    frontend: creating exemple data to be used for now and applying dinamicaly js film list

---
+++ frontend/CineTech/assets/js/data.js
@@ -0,0 +1,66 @@
+categories = [
+		{
+				ID: 1,
+				name: "Action",
+                description: "Films with a lot of fighting"
+		},
+		{
+				ID: 2,
+				name: "Drama",
+                description: "Films that makes you tense",
+		},
+		{
+				ID: 3,
+				name: "Romance",
+                description: "Films about love"
+		},
+		{
+				ID: 4,
+				name: "Terror",
+                description: "Scary films... hhuuuuu!"
+		}
+]
+
+films = [
+		{
+				ID: 1,
+				title: 'Robocop',
+				category: 'Action',
+				description: 'A cop that comes back to life as a robot',
+                cover: 'img/film.webp',
+                link: '"link do trailer"',
+                launch: '01/01/2001',
+                duration: '180min'
+		},
+		{
+				ID: 2,
+				title: '007',
+				category: 'Drama',
+				description: 'A spy that do spy stuff',
+                cover: 'img/film2.webp',
+                link: '"link do trailer"',
+                launch: '01/01/2001',
+                duration: '180min'
+		},
+		{
+				ID: 3,
+				title: 'Before I met you',
+				category: 'Romance',
+				description: 'A paralised guy that falls in love with his nurse',
+                cover: 'img/film3.webp',
+                link: '"link do trailer"',
+                launch: '01/01/2001',
+                duration: '180min'
+		},
+		{
+				ID: 4,
+				title: 'IT - A Coisa',
+				category: 'Terror',
+				description: 'A alien that kills children',
+                cover: 'img/film2.webp',
+                link: '"link do trailer"',
+                launch: '01/01/2001',
+                duration: '180min'
+		},
+]
+
